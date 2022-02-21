60478d354e88363a586838ba877287a75ddb4392
anonymous anonymous <ethan.blumer@kellogg.northwestern.edu>
2022-02-21 05:40:18 +0000

    Display associated collections on show pages

--- app/controllers/very_bests_controller.rb
+++ app/controllers/very_bests_controller.rb
@@ -42,8 +42,14 @@ class VeryBestsController < ApplicationController
   # DELETE /very_bests/1
   def destroy
     @very_best.destroy
-    redirect_to very_bests_url, notice: 'Very best was successfully destroyed.'
+    message = "VeryBest was successfully deleted."
+    if Rails.application.routes.recognize_path(request.referrer)[:controller] != Rails.application.routes.recognize_path(request.path)[:controller]
+      redirect_back fallback_location: request.referrer, notice: message
+    else
+      redirect_to very_bests_url, notice: message
+    end
   end
+
 
   private
     # Use callbacks to share common setup or constraints between actions.
