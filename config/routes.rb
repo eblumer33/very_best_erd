f6f80ca6b5b0840bb0cfee1956bf621d0fec35db
anonymous anonymous <ethan.blumer@kellogg.northwestern.edu>
2022-02-21 05:39:52 +0000

    Generate users resource with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :users
   resources :dishes
   resources :venues
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
